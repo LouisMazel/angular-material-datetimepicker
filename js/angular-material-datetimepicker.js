5b77bfacba09bed306813795ffa26aedacee3c5d
Mazel <loic@classnco.com>
2016-10-19 18:13:16 +0200

    ngChange support

--- js/angular-material-datetimepicker.js
+++ js/angular-material-datetimepicker.js
@@ -79,8 +79,8 @@
         this.locale = localeString;
       };
     })
-    .directive('mdcDatetimePicker', ['$mdDialog',
-      function ($mdDialog) {
+    .directive('mdcDatetimePicker', ['$mdDialog', '$timeout',
+      function ($mdDialog, $timeout) {
 
         return {
           restrict: 'A',
@@ -97,7 +97,8 @@
             okText: '@',
             lang: '@',
             amText: '@',
-            pmText: '@'
+            pmText: '@',
+            ngChange: '&'
           },
           link: function (scope, element, attrs, ngModel) {
             var isOn = false;
@@ -156,6 +157,7 @@
                 .then(function (v) {
                   scope.currentDate = v ? v._d : v;
                   isOn = false;
+                  $timeout(scope.ngChange);
                 }, function () {
                   isOn = false;
                 })
